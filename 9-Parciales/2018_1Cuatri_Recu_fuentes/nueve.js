b1eff8b6b780f4f65f098082b2ceddb7f4073857
byeldiego <47437163+byeldiego@users.noreply.github.com>
2019-02-28 21:48:56 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,47 @@
 function mostrar()
-{
+{	
+	//marca de celular, tamaño, precio;
+	//1_ necesito saber el mayor precio y la marca
+	//2_ promedio precio
+	//3_ cantidad de celus valen > $2000
+	//4 celular, marca, y tamaño del mas caro
+	//5_ " del mas caro
+	//6_ el primercelular que sale $1000
+	//7_ precio del ultimo celular "lg"
+	//prueba de escritorio
+	//lg 3 $3000
+	//samsung 5 $ 5000
+	//iphone 4 $8000
+	//samsung 4 $1000
+	//lg 5 $1000
+
+
+	var marca;
+	var tamaño;
+	var precio;
+	var contador;
+
+	contador=0;
+
+
+	while()
+	{
+		marca=prompt("ingrese marca");
+		tamaño=prompt("ingrese el tamaño");
+		precio=prompt("ingrese el precio");
+	}
+	
+	
+	if()
+	{
+
+	}
+	else 
+	{
+
+	}
+
+
+
 
 }
